93a67a8cd96079f38e4fe18144af78e5e6a813b9
gotr00t0day <48636787+gotr00t0day@users.noreply.github.com>
2026-05-15 09:18:07 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include "modules/kernelpwn.h"
 
+void banner() {
+    std::cout << RED << BOLD << R"(
+         _nnnn_                      
+        dGGGGMMb     ,"""""""""""""""""""".
+       @p~qp~~qMb    | Linux Kenel Pwned! |
+       M|@||@) M|   _;....................'
+       @,----.JM| -'
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    `.       | `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMM|   .'
+     `-'       `--' 
+                                                                        
+)" << RESET;
+    std::cout << CYAN << "                  Linux Kernel Vulnerability Scanner v1.3" << RESET << std::endl;
+    std::cout << YELLOW << "                      Author: c0d3Ninja" << RESET << std::endl;
+    std::cout << BOLD << "========================================================================" << RESET << std::endl;
+    std::cout << "\n\n";
+}
+
 int main() {
+    banner();
     checkVuln();
     return 0;
 }
